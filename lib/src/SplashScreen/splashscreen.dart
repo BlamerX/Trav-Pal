be5e7237cf5b109be3e98492307496735c790cbe
BlamerX <blamerx08@gmail.com>
2023-02-08 22:19:25 +0530

    Added animated Splash Screen

--- lib/src/SplashScreen/splashscreen.dart
+++ lib/src/SplashScreen/splashscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-
+import 'package:video_player/video_player.dart';
 import '../Hompage/homepage.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,9 +12,29 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
+    // Timer(const Duration(seconds: 5), () {
+    //   Navigator.of(context).pushReplacement(
+    //     MaterialPageRoute(
+    //       builder: (_) => const HomePage(),
+    //     ),
+    //   );
+    // });
+
+    _controller = VideoPlayerController.asset('assets/logo/Travpal.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      })
+      ..setVolume(0.0);
+    _playVideo();
+  }
+
+  void _playVideo() async {
+    _controller.play();
     Timer(const Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
@@ -24,17 +44,24 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
+  void disposal() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/images/mobin_dark_image.png"),
-          ],
-        ),
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(
+                  _controller,
+                ),
+              )
+            : Container(),
       ),
     );
   }
